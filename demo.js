3fa1e1335016093cfa8a87905bdb9061795091e4
mattking17 <mking@mking.me>
2014-11-07 16:30:54 +0100

    update demo

--- demo.js
+++ demo.js
@@ -6,11 +6,17 @@
     return Math.round(Math.random()*num);
   }
 
+  function randomColor() {
+    var colors = ['aqua', 'orchid', 'crimson'];
+    return colors[Math.ceil(Math.random()*colors.length-1)];
+  }
+
   function generateRandomEntities(num, axis) {
     var elements = [];
     for (var i = 0; i < num; i++) {
       var c = document.createElement('div');
       c.classList.add('entity');
+      c.classList.add(randomColor());
       c.style.width = c.style.height = (randomWhole(200) + 10) + 'px';
       c.style.left = randomWhole(window.innerWidth) + 'px';
       c.style.top = '50%';
@@ -21,7 +27,7 @@
   }
 
   function generateWave() {
-    var elements = generateRandomEntities(20);
+    var elements = generateRandomEntities(randomWhole(20)+10);
     var parent = document.body;
     for (var i = 0; i < elements.length; i++) {
       parent.appendChild(elements[i]);
