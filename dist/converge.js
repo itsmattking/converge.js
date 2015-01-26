5a6ede7b38685805c74c1c1806894c37fb44c3fe
mattking17 <mking@mking.me>
2015-01-26 17:07:27 +0100

    update build number

--- dist/converge.js
+++ dist/converge.js
@@ -1,5 +1,5 @@
 /**
- * converge.js 0.5 Copyright (c) 2015, Matt King (mking@mking.me)
+ * converge.js 0.5.1 Copyright (c) 2015, Matt King (mking@mking.me)
  * Available via the MIT license.
  * see: https://github.com/mattking17/converge.js for details
  */
